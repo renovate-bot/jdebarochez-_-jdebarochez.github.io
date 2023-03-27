9504f1d4d331c8292673370bab642bdb888265bc
Jean de Barochez <jean.barochez@gmail.com>
2023-03-27 22:22:20 +0200

    wip

---
+++ components/DocumentDrivenNotFound.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Got lost?</h1>
+    <p> Tell me what you were looking for.</p>
+  </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', '@nuxt/ui'],
+  modules: ['@nuxt/content', 
+  // '@nuxt/ui'
+],
   content: {
     documentDriven: true
   },
