432b69169f9440eaf29670c9e6e28d74281842e5
Jean de Barochez <jean.barochez@gmail.com>
2023-06-24 22:56:10 +0200

    feat(social): add rel me for mastodon

--- app.config.ts
+++ app.config.ts
@@ -6,24 +6,24 @@ export default defineAppConfig({
       src: '/profile.jpeg',
       alt: 'An image showcasing my project.',
       width: 400,
-      height: 300
+      height: 300,
     },
     header: {
       position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
         path: '/profile.jpeg', // path of the logo
         pathDark: '/profile.jpeg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'Me.' // alt of the logo
-      }
+        alt: 'Me.', // alt of the logo
+      },
     },
     footer: {
       credits: {
         enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        repository: 'https://www.github.com/nuxt-themes/alpine', // our github repository
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: 'Follow me on', // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
       twitter: 'jdebarochez',
@@ -31,15 +31,16 @@ export default defineAppConfig({
         icon: 'simple-icons:mastodon',
         label: 'mastodon',
         href: 'https://mastodon.social/@jdebarochez',
+        rel: 'me',
       },
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
-        href: 'https://www.linkedin.com/jdebarochez'
-      }
+        href: 'https://www.linkedin.com/jdebarochez',
+      },
     },
     form: {
-      successMessage: 'Message sent. Thank you!'
-    }
-  }
+      successMessage: 'Message sent. Thank you!',
+    },
+  },
 })
